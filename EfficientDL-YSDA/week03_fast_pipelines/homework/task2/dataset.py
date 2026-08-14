b7f6f80e29774cffe1fe13dd83161e46886397e4
roman <romanbokhyan@gmail.com>
2026-08-15 02:32:27 +0400

    Complete task 2 batching pipelines

--- EfficientDL-YSDA/week03_fast_pipelines/homework/task2/dataset.py
+++ EfficientDL-YSDA/week03_fast_pipelines/homework/task2/dataset.py
@@ -1,143 +1,238 @@
-from typing import Optional
+import random  # for random order of samples and batches
+from typing import Optional  # for max length which can be none
 
-import torch
-from torch.utils.data.dataset import Dataset
-from torch.utils.data import Sampler, IterableDataset
-from transformers import AutoTokenizer
+import torch  # for tensors and tensor operations
+from torch.utils.data.dataset import Dataset  # base class for usual datasets
+from torch.utils.data import Sampler, IterableDataset  # base classes for sampler and packed dataset
+from transformers import AutoTokenizer  # bert tokenizer from hugging face
 
-MAX_LENGTH = 640
+
+MAX_LENGTH = 640  # maximum sequence length from task
 TOKENIZER = AutoTokenizer.from_pretrained(
     "bert-base-uncased"
-)  # basically our tokenizer
-PAD_TOKEN_ID = TOKENIZER.pad_token_id  # pad token
+)  # tokenizer which is used for all four modes
+PAD_TOKEN_ID = TOKENIZER.pad_token_id  # id of bert pad token
 
 
-class BrainDataset(Dataset):
+class BrainDataset(Dataset):  # dataset which pads every sample to fixed length
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
-        self.max_length = max_length  # for storing max length
-        self.tokenizer = TOKENIZER  # tokenizer is shared between all datasets
-        self.pad_token_id = PAD_TOKEN_ID  # id we use to pad short sequences
+        self.max_length = max_length  # storing fixed max length
+        self.tokenizer = TOKENIZER  # storing tokenizer inside dataset
+        self.pad_token_id = PAD_TOKEN_ID  # storing pad id inside dataset
 
-        self.texts = []  # for storing non empty texts from wiki
-        with open(data_path, encoding="utf-8") as file:
-            for line in file:
-                line = line.strip()  # removing extra white spaces
-                if line != "":  # check if something located there
-                    self.texts.append(line)  # appending text
+        self.texts = []  # list for non empty wiki texts
+        with open(data_path, encoding="utf-8") as file:  # opening train file
+            for line in file:  # reading file line by line
+                line = line.strip()  # removing spaces and new line symbol
+                if line != "":  # checking that line is not empty
+                    self.texts.append(line)  # saving good line
 
-    def __getitem__(self, idx: int):
-        text = self.texts[idx]  # taking needed part from the text
-        tokens = self.tokenizer.tokenize(
-            text
-        )  # depending on tokenizer we tokenize our text (big sentence is somehow splitted into several parts)
-        token_ids = self.tokenizer.convert_tokens_to_ids(
-            tokens
-        )  # model works with ids -> takes unique id for each unique token from bert dict
-        token_ids = torch.tensor(
-            token_ids, dtype=torch.long
-        )  # chat gpt told me that we need exactly torch long for out nn embedding
+    def __getitem__(self, idx: int):  # returning one padded lm sample
+        text = self.texts[idx]  # taking text by its index
+        tokens = self.tokenizer.tokenize(text)  # splitting text into bert tokens
+        token_ids = self.tokenizer.convert_tokens_to_ids(tokens)  # converting tokens to ids
+        token_ids = torch.tensor(token_ids, dtype=torch.long)  # embedding needs long ids
+        token_ids = token_ids[: self.max_length + 1]  # one extra token is needed for target
 
-        token_ids = token_ids[
-            : self.max_length + 1
-        ]  # lm solves ntp task. imagine we have ids=[1,2,3,4] so input [1,2,3] and target [2,3,4]. to make it work we need to be asureed that shift is considered in indexes
+        input_ids = token_ids[:-1]  # model input without last token
+        targets = token_ids[1:]  # target shifted by one token
 
-        input_ids = token_ids[:-1]
-        targets = token_ids[1:]
-
-        padding_length = self.max_length - len(input_ids)  # how much pads we need
+        padding_length = self.max_length - len(input_ids)  # amount of pads we need
         padding = torch.full(
-            (padding_length,), self.pad_token_id, dtype=torch.long
-        )  # tensor of needed dim with pad ids
+            (padding_length,),
+            self.pad_token_id,
+            dtype=torch.long,
+        )  # tensor with needed amount of pad ids
 
-        input_ids = torch.cat((input_ids, padding))
-        targets = torch.cat((targets, padding))
-        return input_ids, targets
+        input_ids = torch.cat((input_ids, padding))  # padding model input to fixed length
+        targets = torch.cat((targets, padding))  # padding target to the same length
+        return input_ids, targets  # returning one ready sample
 
-    def __len__(self) -> int:  # to get a length of a sequence
-        return len(self.texts)
-
-
-class BigBrainDataset(BrainDataset):
-    def __getitem__(self, idx: int):
-        text = self.texts[idx]  # taking needed part from the text
-        tokens = self.tokenizer.tokenize(
-            text
-        )  # depending on tokenizer we tokenize our text (big sentence is somehow splitted into several parts)
-        token_ids = self.tokenizer.convert_tokens_to_ids(
-            tokens
-        )  # model works with ids -> takes unique id for each unique token from bert dict
-        token_ids = torch.tensor(
-            token_ids, dtype=torch.long
-        )  # chat gpt told me that we need exactly torch long for out nn embedding
-
-        token_ids = token_ids[
-            : self.max_length + 1
-        ]  # lm solves ntp task. imagine we have ids=[1,2,3,4] so input [1,2,3] and target [2,3,4]. to make it work we need to be asureed that shift is considered in indexes
-
-        input_ids = token_ids[:-1]
-        targets = token_ids[1:]
-        return input_ids, targets
+    def __len__(self):  # returning number of texts in dataset
+        return len(self.texts)  # dataloader uses this value
 
 
-class UltraBigBrainDataset(Dataset):
-    def __init__(self, data_path: str, max_length: int = MAX_LENGTH, n_bins: int = 1):
-        pass
+class BigBrainDataset(BrainDataset):  # dataset where collate fn will make padding
+    def __getitem__(self, idx: int):  # returning one sample without padding
+        text = self.texts[idx]  # taking text by its index
+        tokens = self.tokenizer.tokenize(text)  # splitting text into bert tokens
+        token_ids = self.tokenizer.convert_tokens_to_ids(tokens)  # converting tokens to ids
+        token_ids = torch.tensor(token_ids, dtype=torch.long)  # embedding needs long ids
+        token_ids = token_ids[: self.max_length + 1]  # truncating long text
 
-    def __getitem__(self, idx: int):
-        pass
+        input_ids = token_ids[:-1]  # model input without last token
+        targets = token_ids[1:]  # target shifted by one token
+        return input_ids, targets  # returning variable length sample
 
 
-class UltraDuperBigBrainDataset(IterableDataset):
+class UltraBigBrainDataset(Dataset):  # dataset which stores samples and their lengths
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
-        pass
+        self.max_length = max_length  # storing maximum sequence length
+        self.tokenizer = TOKENIZER  # storing tokenizer inside dataset
+        self.pad_token_id = PAD_TOKEN_ID  # storing pad id for collate fn
 
-    def __iter__(self):
-        pass
+        self.samples = []  # list with already tokenized input and target pairs
+        self.indices_by_length = {}  # hash table with length and sample indices
+
+        with open(data_path, encoding="utf-8") as file:  # opening train file
+            for line in file:  # reading file line by line
+                line = line.strip()  # removing extra spaces
+                if line == "":  # checking if line is empty
+                    continue  # skipping empty line
+
+                tokens = self.tokenizer.tokenize(line)  # splitting text into tokens
+                token_ids = self.tokenizer.convert_tokens_to_ids(tokens)  # taking ids from vocab
+                token_ids = torch.tensor(token_ids, dtype=torch.long)  # creating long tensor
+                token_ids = token_ids[: self.max_length + 1]  # truncating long sequence
+
+                if len(token_ids) < 2:  # checking that lm pair can be created
+                    continue  # skipping too short sample
+
+                input_ids = token_ids[:-1]  # creating model input
+                targets = token_ids[1:]  # creating shifted target
+                sample_idx = len(self.samples)  # index of new sample
+                self.samples.append((input_ids, targets))  # saving tokenized sample
+
+                sample_length = len(input_ids)  # length which sampler will use
+                if sample_length not in self.indices_by_length:  # checking length in hash table
+                    self.indices_by_length[sample_length] = []  # creating list for new length
+                self.indices_by_length[sample_length].append(sample_idx)  # saving sample index
+
+    def __getitem__(self, idx: int):  # returning already tokenized sample
+        return self.samples[idx]  # taking pair by its index
+
+    def __len__(self):  # returning number of saved samples
+        return len(self.samples)  # dataloader and sampler use this value
+
+
+class UltraBigBrainBatchSampler(Sampler):  # sampler which groups close sequence lengths
+    def __init__(self, dataset: UltraBigBrainDataset, batch_size: int, k: int):
+        self.batch_size = batch_size  # storing wanted batch size
+        self.batches = []  # list with already created batches
+
+        buckets = {}  # hash table with bucket id and sample indices
+        bucket_width = k + 1  # this width guarantees max length difference k
+
+        for length, indices in dataset.indices_by_length.items():  # reading exact lengths
+            bucket_id = (length - 1) // bucket_width  # finding range for current length
+            if bucket_id not in buckets:  # checking bucket in hash table
+                buckets[bucket_id] = []  # creating new bucket
+            buckets[bucket_id].extend(indices)  # adding all indices with current length
+
+        for indices in buckets.values():  # processing every length bucket
+            random.shuffle(indices)  # making random order inside bucket
+            for start in range(0, len(indices), self.batch_size):  # splitting bucket into batches
+                batch = indices[start : start + self.batch_size]  # taking one batch part
+                self.batches.append(batch)  # saving ready batch
+
+        random.shuffle(self.batches)  # making random order of all ready batches
+
+    def __len__(self):  # returning number of batches
+        return len(self.batches)  # dataloader uses this value
+
+    def __iter__(self):  # creating iterator over ready batches
+        return iter(self.batches)  # iter call does not process whole dataset again
+
+
+class UltraDuperBigBrainDataset(IterableDataset):  # dataset which packs many texts together
+    def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
+        self.max_length = max_length  # storing pack length
+        self.tokenizer = TOKENIZER  # storing tokenizer
+        self.pad_token_id = PAD_TOKEN_ID  # storing pad id
+
+        self.texts = []  # list with non empty wiki texts
+        with open(data_path, encoding="utf-8") as file:  # opening train file
+            for line in file:  # reading file line by line
+                line = line.strip()  # removing spaces and new line symbol
+                if line != "":  # checking that line is not empty
+                    self.texts.append(line)  # saving text
+
+    def _create_pack(self, input_ids, targets, borders):  # creating tensors and mask for one pack
+        real_length = len(input_ids)  # number of real tokens before padding
+        padding_length = self.max_length - real_length  # amount of pads for last pack
+
+        input_ids += [self.pad_token_id] * padding_length  # padding packed model input
+        targets += [self.pad_token_id] * padding_length  # padding packed target
+
+        attention_mask = torch.full(
+            (self.max_length, self.max_length),
+            float("-inf"),
+        )  # at first all attention connections are blocked
+
+        for start, end in borders:  # processing every original text inside pack
+            block_length = end - start  # taking length of current text part
+            causal_block = torch.triu(
+                torch.full((block_length, block_length), float("-inf")),
+                diagonal=1,
+            )  # causal mask blocks future tokens inside current text
+            attention_mask[start:end, start:end] = causal_block  # adding block to common mask
+
+        for idx in range(real_length, self.max_length):  # processing padding positions
+            attention_mask[idx, idx] = 0  # pad position can attend only to itself
+
+        return (
+            torch.tensor(input_ids, dtype=torch.long),
+            torch.tensor(targets, dtype=torch.long),
+            attention_mask,
+        )  # returning packed input target and correct attention mask
+
+    def __iter__(self):  # creating packed samples one by one
+        packed_inputs = []  # tokens from different texts inside current pack
+        packed_targets = []  # shifted targets inside current pack
+        borders = []  # start and end positions of every text inside pack
+
+        for text in self.texts:  # processing every wiki text
+            tokens = self.tokenizer.tokenize(text)  # splitting text into tokens
+            token_ids = self.tokenizer.convert_tokens_to_ids(tokens)  # converting tokens to ids
+            token_ids = token_ids[: self.max_length + 1]  # truncating very long text
+
+            if len(token_ids) < 2:  # checking that lm pair can be created
+                continue  # skipping too short text
+
+            input_ids = token_ids[:-1]  # creating model input
+            targets = token_ids[1:]  # creating shifted target
+            free_space = self.max_length - len(packed_inputs)  # free positions in current pack
+            part_length = min(len(input_ids), free_space)  # amount of current text which fits
+
+            start = len(packed_inputs)  # start position of current text part
+            end = start + part_length  # end position of current text part
+            packed_inputs.extend(input_ids[:part_length])  # adding input tokens into pack
+            packed_targets.extend(targets[:part_length])  # adding target tokens into pack
+            borders.append((start, end))  # saving text borders for attention mask
+
+            if len(packed_inputs) == self.max_length:  # checking that pack is full
+                yield self._create_pack(packed_inputs, packed_targets, borders)  # returning full pack
+                packed_inputs = []  # starting new input pack
+                packed_targets = []  # starting new target pack
+                borders = []  # starting new list of borders
+
+        if len(packed_inputs) > 0:  # checking if last not full pack exists
+            yield self._create_pack(packed_inputs, packed_targets, borders)  # returning last pack
 
 
 def collate_fn(
     batch: list[tuple[torch.Tensor, torch.Tensor]],
     max_length: Optional[int] = MAX_LENGTH,
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """
-    Pad each sequence of the incoming sequences list
-    :param batch: a list of the objects received from the dataset by __getitem__
-    :param max_length: maximum sequence length to pad to (for "Brain" approach only)
-    :return: tuple of padded sequences and corresponding training targets
-    """
-    if max_length is None:
-        target_length = max(
-            len(input_ids) for input_ids, _ in batch
-        )  # if its not fixed pad then we need max len in batch
-    else:
-        target_length = max_length  # for brain mode we always pad to fixed max len
+):  # manually padding samples and stacking them into batch
+    if max_length is None:  # big brain and ultra big brain mode
+        target_length = max(len(input_ids) for input_ids, _ in batch)  # max len in batch
+    else:  # brain mode with fixed length
+        target_length = max_length  # always using given fixed length
 
-    padded_inputs = []  # for storing padded model inputs
-    padded_targets = []  # for storing padded next token targets
-    for input_ids, targets in batch:
-        padding_length = target_length - len(input_ids)  # how many pads current sequence needs
+    padded_inputs = []  # list with padded model inputs
+    padded_targets = []  # list with padded targets
 
+    for input_ids, targets in batch:  # processing every sample inside batch
+        padding_length = target_length - len(input_ids)  # amount of needed pads
         padding = torch.full(
             (padding_length,),
             PAD_TOKEN_ID,
             dtype=torch.long,
-        )  # tensor with needed amount of pad ids
+        )  # creating pad tensor for current sample
 
-        padded_inputs.append(torch.cat((input_ids, padding)))  # pad model input
-        padded_targets.append(torch.cat((targets, padding)))  # pad corresponding target
+        padded_inputs.append(torch.cat((input_ids, padding)))  # saving padded input
+        padded_targets.append(torch.cat((targets, padding)))  # saving padded target
 
-    inputs_batch = torch.stack(padded_inputs)  # list of vectors -> one batch matrix
-    targets_batch = torch.stack(padded_targets)  # targets should have the same batch shape
-    return inputs_batch, targets_batch
-
-
-class UltraBigBrainBatchSampler(Sampler):
-
-    def __init__(self, batch_size: int, max_length: Optional[int] = MAX_LENGTH):
-        pass
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
-        pass
+    inputs_batch = torch.stack(padded_inputs)  # list of inputs becomes batch matrix
+    targets_batch = torch.stack(padded_targets)  # list of targets becomes batch matrix
+    return inputs_batch, targets_batch  # returning ready batch
--- EfficientDL-YSDA/week03_fast_pipelines/homework/task2/run_epoch.py
+++ EfficientDL-YSDA/week03_fast_pipelines/homework/task2/run_epoch.py
@@ -1,18 +1,189 @@
-from enum import Enum
+import statistics  # for mean and median batch time
+import time  # for measuring forward pass time
+from enum import Enum  # for readable names of four batching modes
+from functools import partial  # for passing max length into collate fn
 
-import torch
+import pandas as pd  # for final table with all benchmark results
+import torch  # for model cuda and tensor operations
+from torch.utils.data import DataLoader  # for creating batches from datasets
+
+from dataset import (
+    TOKENIZER,
+    BigBrainDataset,
+    BrainDataset,
+    UltraBigBrainBatchSampler,
+    UltraBigBrainDataset,
+    UltraDuperBigBrainDataset,
+    collate_fn,
+)  # importing all dataset modes from our file
+from transformer import (
+    TransformerModel,
+    generate_square_subsequent_mask,
+)  # importing given transformer and causal mask
 
 
-class DataMode(Enum):
-    BRAIN = 1
-    BIG_BRAIN = 2
-    ULTRA_BIG_BRAIN = 3
-    ULTRA_DUPER_BIG_BRAIN = 4
+class DataMode(Enum):  # readable values for choosing batching approach
+    BRAIN = 1  # fixed padding to 640
+    BIG_BRAIN = 2  # padding to max length inside current batch
+    ULTRA_BIG_BRAIN = 3  # batches with close sequence lengths
+    ULTRA_DUPER_BIG_BRAIN = 4  # many texts packed into one sequence
 
 
-def get_gpt2_model() -> torch.nn.Module:
-    pass
+def get_gpt2_model():  # creating small gpt like model from task
+    model = TransformerModel(
+        ntoken=TOKENIZER.vocab_size,
+        d_model=1024,
+        nhead=8,
+        d_hid=1024,
+        nlayers=1,
+        dropout=0.0,
+    )  # one transformer layer with hidden size 1024 and 8 heads
+    return model  # returning model without moving it to device yet
 
 
-def run_epoch(data_mode: DataMode) -> None:
-    pass
+def _create_loader(data_mode, data_path, batch_size, k):  # creating loader for chosen mode
+    if data_mode == DataMode.BRAIN:  # fixed padding mode
+        dataset = BrainDataset(data_path)  # every sample is already padded to 640
+        loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=True,
+        )  # default collate only stacks already equal tensors
+
+    elif data_mode == DataMode.BIG_BRAIN:  # padding inside collate fn mode
+        dataset = BigBrainDataset(data_path)  # samples have different lengths
+        loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=partial(collate_fn, max_length=None),
+        )  # none tells collate fn to use max length of current batch
+
+    elif data_mode == DataMode.ULTRA_BIG_BRAIN:  # smart length buckets mode
+        dataset = UltraBigBrainDataset(data_path)  # dataset stores lengths and sample indices
+        batch_sampler = UltraBigBrainBatchSampler(
+            dataset,
+            batch_size,
+            k,
+        )  # sampler creates batches where max length difference is at most k
+        loader = DataLoader(
+            dataset,
+            batch_sampler=batch_sampler,
+            collate_fn=partial(collate_fn, max_length=None),
+        )  # batch sampler already controls batch size and sample order
+
+    else:  # packed ultra duper mode
+        dataset = UltraDuperBigBrainDataset(data_path)  # dataset yields ready packed sequences
+        loader = DataLoader(
+            dataset,
+            batch_size=None,
+        )  # every yielded pack is already one complete batch
+
+    return loader  # returning loader for warmup and benchmark
+
+
+def _prepare_batch(batch, data_mode, device):  # moving batch and mask to device
+    if data_mode == DataMode.ULTRA_DUPER_BIG_BRAIN:  # packed mode has custom mask
+        input_ids, _, attention_mask = batch  # target is not needed without backward
+        input_ids = input_ids.unsqueeze(1).to(device)  # adding batch dimension
+        attention_mask = attention_mask.to(device)  # moving packed block mask to device
+    else:  # first three modes use usual causal mask
+        input_ids, _ = batch  # target is not needed for forward only benchmark
+        input_ids = input_ids.transpose(0, 1).to(device)  # batch first to sequence first
+        attention_mask = generate_square_subsequent_mask(
+            len(input_ids)
+        ).to(device)  # blocking attention to future tokens
+
+    return input_ids, attention_mask  # returning everything model needs
+
+
+def run_epoch(
+    data_mode: DataMode,
+    data_path: str,
+    batch_size: int = 8,
+    k: int = 5,
+    warmup_batches: int = 5,
+):  # running one forward only epoch and collecting batch time
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )  # using gpu when it exists
+    loader = _create_loader(
+        data_mode,
+        data_path,
+        batch_size,
+        k,
+    )  # creating loader for selected batching approach
+    model = get_gpt2_model().to(device)  # creating model and moving it to device
+    model.eval()  # disabling train mode behavior like dropout
+
+    with torch.no_grad():  # gradients are not needed in this task
+        for batch_idx, batch in enumerate(loader):  # taking first batches for warmup
+            if batch_idx == warmup_batches:  # checking warmup limit
+                break  # stopping warmup before real benchmark
+            input_ids, attention_mask = _prepare_batch(
+                batch,
+                data_mode,
+                device,
+            )  # preparing warmup batch
+            model(input_ids, attention_mask)  # running warmup forward
+
+    if device.type == "cuda":  # cuda operations are asynchronous
+        torch.cuda.synchronize()  # waiting until warmup is fully done
+
+    batch_times = []  # list for processing time of every batch
+    with torch.no_grad():  # benchmark also does not need gradients
+        for batch in loader:  # running full mock epoch
+            input_ids, attention_mask = _prepare_batch(
+                batch,
+                data_mode,
+                device,
+            )  # preparing current batch before timer
+
+            if device.type == "cuda":  # checking if asynchronous cuda is used
+                torch.cuda.synchronize()  # waiting before starting timer
+            start_time = time.perf_counter()  # saving start time
+
+            model(input_ids, attention_mask)  # running only model forward
+
+            if device.type == "cuda":  # checking if asynchronous cuda is used
+                torch.cuda.synchronize()  # waiting until forward is finished
+            batch_time = (time.perf_counter() - start_time) * 1000  # seconds to milliseconds
+            batch_times.append(batch_time)  # saving current batch time
+
+    mode_name = data_mode.name.lower()  # readable name for result table
+    if data_mode == DataMode.ULTRA_BIG_BRAIN:  # this mode has different k values
+        mode_name = f"{mode_name}_k_{k}"  # adding k to result name
+
+    return {
+        "mode": mode_name,
+        "min_ms": min(batch_times),
+        "max_ms": max(batch_times),
+        "mean_ms": statistics.mean(batch_times),
+        "median_ms": statistics.median(batch_times),
+    }  # returning all statistics required by task
+
+
+def run_all(data_path: str, batch_size: int = 8):  # running all required benchmark modes
+    results = []  # list with rows for final dataframe
+
+    results.append(
+        run_epoch(DataMode.BRAIN, data_path, batch_size)
+    )  # fixed padding result
+    results.append(
+        run_epoch(DataMode.BIG_BRAIN, data_path, batch_size)
+    )  # current batch padding result
+
+    for k in [1, 5, 10, 20, 50]:  # all k values required by task
+        results.append(
+            run_epoch(
+                DataMode.ULTRA_BIG_BRAIN,
+                data_path,
+                batch_size,
+                k=k,
+            )
+        )  # adding result for current k
+
+    results.append(
+        run_epoch(DataMode.ULTRA_DUPER_BIG_BRAIN, data_path, batch_size)
+    )  # packed mode result
+    return pd.DataFrame(results)  # converting list of results into report table
